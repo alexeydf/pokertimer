877a1be5b799028f9b74a13ea4e3ff60e62d62f8
alexeydf <74327997+alexeydf@users.noreply.github.com>
2024-05-19 19:21:50 -0300

    Subida dos primeiros arquivos criados

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,133 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="number" v-model="tempoEscolhido">
+    <button @click="adicionar" class="btn">+</button><button class="btn">-</button>
+    <p>{{ minutos }}:{{ segundos }}</p>
+    <p>{{ blind }}</p>
+    <button v-show="start" @click="startTimer" class="btn">start</button>
+    <button @click="pauseTimer" class="btn">pause</button>
+    <button @click="finalizar" class="btn">finalizar</button>
+    <button @click="proximo" class="btn">proximo</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './styles/global.css';
+import { blinds } from './blinds/blinds';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+  },
+  data(){
+    return{
+      tempoEscolhido: 0,
+      tempoRegressivo: 0,
+      fim: null,
+      intervalo: null,
+      minutos: 0,
+      segundos: 0,
+      pause: false,
+      start: true,
+      blind: null,
+      tempoFaltante: null,
+      index: 0,
+      blinds 
+    }
+   },
+   beforeDestroy(){
+    this.resetValores();
+   },
+  methods:{
+    adicionar(){
+      this.tempoEscolhido = this.tempoEscolhido + 5;
+    },
+    startTimer(){
+      if(this.tempoEscolhido <= 0){
+        return false;
+      }
+
+      if(this.pause){
+        this.fim = new Date().getTime() + this.tempoFaltante;
+        this.pause = false;
+        this.start = false;
+      }else{
+        this.start = false;
+        this.tempoRegressivo = this.tempoEscolhido * 60 * 1000;
+        this.fim = new Date().getTime() + this.tempoRegressivo;
+      }
+
+      this.updateTimer();
+      this.intervalo = setInterval(this.updateTimer, 1000);
+
+    },
+    updateTimer(){
+      const now = new Date().getTime();
+      const distance = this.fim - now
+
+      if(distance < 0){
+      
+        if(this.index+1 >= this.blinds.length){
+          this.resetValores();
+        }else{
+          this.index++;
+          this.startTimer();
+        }
+        
+      }else{
+        this.tempoFaltante = distance;
+        this.minutos = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        this.segundos = Math.floor((distance % (1000 * 60)) / 1000);
+        this.blind = this.blinds[this.index];
+
+        if(this.minutos < 10){
+          this.minutos = `0${this.minutos}`;
+        }
+        if(this.segundos < 10){
+          this.segundos = `0${this.segundos}`;
+        }
+      }
+
+      
+    },
+    pauseTimer(){
+      clearInterval(this.intervalo);
+      this.pause = true;
+      this.start = true;
+    },
+    finalizar(){
+      this.resetValores();
+    },
+    resetValores(){
+      clearInterval(this.intervalo);
+      this.tempoFaltante = null;
+      this.tempoRegressivo = 0;
+      this.minutos = 0;
+      this.segundos = 0;
+      this.start = true;
+      this.pause = false;
+      this.index = 0;
+      this.tempoEscolhido = 0;
+      this.fim = null;
+      this.intervalo = null;
+      this.blind = null;
+    },
+    proximo(){
+      clearInterval(this.intervalo);
+      if(this.index + 1 < this.blinds.length){
+        this.index++;
+        this.pause = false;
+        this.startTimer();
+      }else{
+        this.resetValores();
+      }
+      
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/blinds/blinds.js
@@ -0,0 +1,7 @@
+export const blinds = [
+  {level: 1, smallBlind: 25, bigBlind: 50},
+  {level: 2, smallBlind: 50, bigBlind: 100},
+  {level: 3, smallBlind: 75, bigBlind: 150},
+  {level: 4, smallBlind: 100, bigBlind: 200},
+  {level: 5, smallBlind: 125, bigBlind: 250}
+]
